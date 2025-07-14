4f63f5ea1d6f7aabb3b172aa9fb9369fabd6c687
Adem Bayazıt <adembayazit@gmail.com>
2025-07-14 22:53:34 +0300

    Add files via upload

---
+++ netlify/functions/log-visitor.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  // IP ve tarih bilgisini al
+  const ip = event.headers['x-nf-client-connection-ip'] || event.headers['x-forwarded-for'];
+  const date = new Date().toISOString();
+  
+  // Logu konsola yazdır (Netlify Dashboard'da görülebilir)
+  console.log("Yeni ziyaretçi:", { ip, date });
+
+  // Başarılı yanıt dön
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ip, date }),
+  };
+};
