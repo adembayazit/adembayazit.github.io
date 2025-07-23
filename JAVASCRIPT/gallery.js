c20623832f7feaa2698b38f9bd82efec505200ab
Adem Bayazıt <adembayazit@gmail.com>
2025-07-23 21:01:49 +0300

    Update gallery.js

--- JAVASCRIPT/gallery.js
+++ JAVASCRIPT/gallery.js
@@ -26,8 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
       img.classList.add("gallery-photo");
       img.alt = "Gallery Photo";
       img.addEventListener("click", () => {
-        modal.style.display = "block";
+        modal.style.display = "flex"; // <== Burayı değiştirdik
         modalImg.src = img.src;
+        document.body.style.overflow = "hidden"; // <== Scroll'u kapat
       });
       galleryGrid.appendChild(img);
     });
@@ -42,9 +43,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
+    document.body.style.overflow = "auto"; // <== Scroll'u geri aç
   });
 
   window.addEventListener("click", e => {
-    if (e.target === modal) modal.style.display = "none";
+    if (e.target === modal) {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto"; // <== Scroll'u geri aç
+    }
   });
 });
