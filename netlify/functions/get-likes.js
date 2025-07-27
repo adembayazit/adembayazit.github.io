6aca1c889b2da8325ef29cd0404adab25695cd48
Adem Bayazıt <adembayazit@gmail.com>
2025-07-27 14:45:31 +0300

    Update get-likes.js

--- netlify/functions/get-likes.js
+++ netlify/functions/get-likes.js
@@ -1,33 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const filePath = path.resolve(__dirname, 'likes.json');
 
-const filePath = path.resolve(__dirname, "likes.json");
-
-exports.handler = async function (event) {
-  const entryId = event.queryStringParameters.id;
-
-  if (!entryId) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Missing entry ID" }),
-    };
-  }
-
-  let likesData = {};
-
-  try {
-    if (fs.existsSync(filePath)) {
-      likesData = JSON.parse(fs.readFileSync(filePath));
-    }
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "File read error" }),
-    };
-  }
-
+exports.handler = async (event) => {
+  const id = event.queryStringParameters?.id;
+  const db = fs.existsSync(filePath) ? JSON.parse(fs.readFileSync(filePath)) : {};
   return {
     statusCode: 200,
-    body: JSON.stringify({ likes: likesData[entryId] || 0 }),
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ likes: db[id] || 0 })
   };
 };
