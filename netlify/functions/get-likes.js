019fdebbc41df2b06bd2734b963b417363448a51
Adem Bayazıt <adembayazit@gmail.com>
2025-07-27 15:37:34 +0300

    Update get-likes.js

--- netlify/functions/get-likes.js
+++ netlify/functions/get-likes.js
@@ -2,25 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event) => {
-  const id = event.queryStringParameters.id;
   const filePath = path.resolve(__dirname, 'likes.json');
+  const likesData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-  try {
-    let likesData = {};
-    if (fs.existsSync(filePath)) {
-      const raw = fs.readFileSync(filePath);
-      likesData = JSON.parse(raw);
-    }
-
-    const likeCount = likesData[id] || 0;
-
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ likes: likeCount })
-    };
-  } catch (err) {
-    console.error("get-likes.js error:", err);
-    return { statusCode: 500, body: "Error reading like count" };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(likesData),
+  };
 };
