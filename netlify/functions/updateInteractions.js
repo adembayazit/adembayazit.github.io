1adb5db404c441ed3a61ced2ba83556a43f307d4
Adem Bayazıt <adembayazit@gmail.com>
2025-08-06 18:43:46 +0300

    Update updateInteractions.js

--- netlify/functions/updateInteractions.js
+++ netlify/functions/updateInteractions.js
@@ -1,9 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
   try {
     const { updatedLikes, updatedPins } = JSON.parse(event.body);
-    const response = await fetch('https://api.jsonbin.io/v3/b/68862fd97b4b8670d8a81945', {
+    const response = await fetch('https://api.jsonbin.io/v3/b/YOUR_BIN_ID', {
       method: 'PUT',
       headers: {
         'X-Master-Key': process.env.JSONBIN_MASTER_KEY,
@@ -13,6 +17,6 @@ exports.handler = async (event) => {
     });
     return { statusCode: 200, body: JSON.stringify(await response.json()) };
   } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: "Güncelleme başarısız" }) };
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   }
 };
