1935f4a8cd50aa5329ca5b971bc4194b90f60b68
Adem Bayazıt <adembayazit@gmail.com>
2025-08-06 20:47:53 +0300

    Update get-entries.js

--- netlify/functions/get-entries.js
+++ netlify/functions/get-entries.js
@@ -2,33 +2,29 @@ const fetch = require("node-fetch");
 
 exports.handler = async function () {
   const BIN_ID = "68933248ae596e708fc2fbbc";
-  const API_KEY = process.env.JSONBIN_MASTER_KEY;
+  const MASTER_KEY = process.env.JSONBIN_MASTER_KEY;
 
   try {
     const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
-      method: 'GET',
       headers: {
-        'X-Master-Key': API_KEY
-      }
+        "X-Master-Key": MASTER_KEY,
+      },
     });
 
     if (!response.ok) {
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: 'JSONBin fetch failed' })
-      };
+      throw new Error(`HTTP ${response.status}`);
     }
 
-    const json = await response.json();
+    const data = await response.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify(json.record) // sadece "record" kısmını al
+      body: JSON.stringify(data),
     };
-
-  } catch (error) {
+  } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: err.message }),
     };
   }
 };
