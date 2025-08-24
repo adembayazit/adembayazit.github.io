673f97d2c9e3ebb5d861a0059bcf6efdb8c9f2a7
Adem Bayazıt <adembayazit@gmail.com>
2025-08-24 12:48:09 +0300

    Update imagekit-auth.js

--- netlify/functions/imagekit-auth.js
+++ netlify/functions/imagekit-auth.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    'Access-Control-Allow-Methods': 'GET,POST, OPTIONS'
   };
 
   // Handle preflight
