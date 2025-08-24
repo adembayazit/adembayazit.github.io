4201874b8eec50c7888b42aaeec22f530e559298
Adem Bayazıt <adembayazit@gmail.com>
2025-08-25 01:11:16 +0300

    Update imagekit-auth.js

--- netlify/functions/imagekit-auth.js
+++ netlify/functions/imagekit-auth.js
@@ -20,34 +20,32 @@ exports.handler = async (event, context) => {
             IMAGEKIT_URL_ENDPOINT 
         } = process.env;
 
-        console.log('Environment Variables:', {
+        console.log('Environment Variables Check:', {
             hasPublicKey: !!IMAGEKIT_PUBLIC_KEY,
             hasPrivateKey: !!IMAGEKIT_PRIVATE_KEY,
-            hasEndpoint: !!IMAGEKIT_URL_ENDPOINT,
-            privateKeyStart: IMAGEKIT_PRIVATE_KEY ? IMAGEKIT_PRIVATE_KEY.substring(0, 10) + '...' : 'MISSING'
+            hasEndpoint: !!IMAGEKIT_URL_ENDPOINT
         });
 
         if (!IMAGEKIT_PUBLIC_KEY || !IMAGEKIT_PRIVATE_KEY || !IMAGEKIT_URL_ENDPOINT) {
             throw new Error('Missing ImageKit environment variables');
         }
 
-        // ImageKit instance'ını DOĞRU şekilde oluştur
-        // SDK otomatik olarak private key'i işler
         const imagekit = new ImageKit({
             publicKey: IMAGEKIT_PUBLIC_KEY,
-            privateKey: IMAGEKIT_PRIVATE_KEY, // RAW private key
+            privateKey: IMAGEKIT_PRIVATE_KEY,
             urlEndpoint: IMAGEKIT_URL_ENDPOINT
         });
 
-        // Authentication parametrelerini al
         const authParams = imagekit.getAuthenticationParameters();
-        // authParams.publicKey = IMAGEKIT_PUBLIC_KEY;
+        
+        // ÖNEMLİ: publicKey'i response'a ekle
+        authParams.publicKey = IMAGEKIT_PUBLIC_KEY;
 
         console.log('Generated Auth Parameters:', {
             token: authParams.token,
             expire: authParams.expire,
             signature: authParams.signature,
-            signatureLength: authParams.signature.length
+            publicKey: authParams.publicKey
         });
 
         return {
