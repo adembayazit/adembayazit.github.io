4862f6636b4d6b4bb3745f62fed13110e9d1884e
Adem Bayazıt <adembayazit@gmail.com>
2025-08-28 14:08:28 +0300

    Update log-visitor.js

--- netlify/functions/log-visitor.js
+++ netlify/functions/log-visitor.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://adembayazit.com',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Content-Type': 'application/json',
