8e07d8a40ec98d085522583e8b1ca6a2ae219ef4
Adem Bayazıt <adembayazit@gmail.com>
2025-09-01 12:41:35 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: data.icon || 'https://adembayazit.com/IMAGES/ab.png',
+    badge: 'https://adembayazit.com/IMAGES/ab.png',
+    vibrate: [200, 100, 200],
+    data: {
+      url: data.url || 'https://adembayazit.com/microblog'
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
